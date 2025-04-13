481621cdae0f7b89f3c22c5b2d9bbe459a550eea
francky <40074567+francky-d@users.noreply.github.com>
2025-04-14 01:18:23 +0200

    wip:fetch application_groups from api

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,14 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const { data: session } = useSession();
+  const [groups, setGroups] = useState([]);
+
+   async function fetchGroup(){
+    const fetchedGroups = await fetch("http://localhost:8000/api/v1/applications-groups")
+     .then(res => res.json())
+        .catch(error => console.log(error));
+     setGroups(fetchedGroups);
+  }
   return (
     <div className="p-6 text-center">
       {session ? (
@@ -12,6 +20,24 @@ export default function Home() {
           <h1 className="text-xl mb-4">Hello, {session.user?.name}</h1>
           <p>{session.user?.id}</p>
           <p>{session.user?.email}</p>
+
+          <button
+              onClick={() => fetchGroup()}
+              className="bg-red-500 text-white px-4 py-2 rounded"
+          >
+           Groups
+          </button>
+
+          <ul>
+            {
+
+                   groups?.length > 0 && groups.map((group: any) => (
+                   <li key={group.id}>{group?.id}</li>
+                   ))
+
+            }
+          </ul>
+
           <button
             onClick={() => signOut()}
             className="bg-red-500 text-white px-4 py-2 rounded"
