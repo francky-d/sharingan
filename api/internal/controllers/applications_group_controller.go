6fc9c944f24600d831a22bdd295c2ac38f2fa9ed
francky <40074567+francky-d@users.noreply.github.com>
2025-05-16 02:23:36 +0200

    chore: refactor how response struct is created and used

--- api/internal/controllers/applications_group_controller.go
+++ api/internal/controllers/applications_group_controller.go
@@ -14,23 +14,18 @@ import (
 // swagger embed files
 
 type ApplicationGroupController struct {
-	response.Response
 	logger *zap.Logger
 }
 
 var controllerInstance *ApplicationGroupController
 
 func NewApplicationGroupController(logger *zap.Logger) *ApplicationGroupController {
-	if controllerInstance == nil {
-		controllerInstance = &ApplicationGroupController{logger: logger}
-	}
-
-	return controllerInstance
+	return &ApplicationGroupController{logger: logger}
 }
 
 //	@BasePath	/api/v1
 
-// PingExample godoc
+// PingExample godoc'
 //
 //	@Summary	ping example
 //	@Schemes
@@ -40,22 +35,25 @@ func NewApplicationGroupController(logger *zap.Logger) *ApplicationGroupControll
 //	@Produce		json
 //	@Success		200	{string}	Helloworld
 //	@Router			/example/helloworld [get]
-func (controller *ApplicationGroupController) Index(c *gin.Context) {
+func (controller *ApplicationGroupController) Index(ctx *gin.Context) {
+	resp := response.NewResponse(ctx)
+
 	var applicationsGroups []models.ApplicationGroup
 
 	database.DbConnection().Db().Find(&applicationsGroups)
 
-	controller.SendSuccessResponse(gin.H{
+	resp.Success().SendOk(gin.H{
 		"application_groups": applicationsGroups,
 	})
 	return
 }
 
-func (controller *ApplicationGroupController) Store(c *gin.Context) {
+func (controller *ApplicationGroupController) Store(ctx *gin.Context) {
 	var group models.ApplicationGroup
+	resp := response.NewResponse(ctx)
 
-	if err := c.ShouldBindJSON(&group); err != nil {
-		controller.SendBadRequestWithErr(err)
+	if err := ctx.ShouldBindJSON(&group); err != nil {
+		resp.Error().SendBadRequestWithErr(err)
 		return
 	}
 
@@ -64,26 +62,27 @@ func (controller *ApplicationGroupController) Store(c *gin.Context) {
 	result := database.DbConnection().Db().Create(&group)
 	if result.Error != nil {
 		controller.logger.Error("Error on storing application group", zap.Error(result.Error))
-		controller.SendInternalServerWithErr()
+		resp.Error().SendInternalServerWithErr()
 		return
 	}
 
-	controller.SendCreatedResponse(gin.H{
+	resp.Success().SendCreated(gin.H{
 		"id": group.ID,
 	})
 
 	return
 }
 
-func (controller *ApplicationGroupController) Show(c *gin.Context) {
+func (controller *ApplicationGroupController) Show(ctx *gin.Context) {
+	resp := response.NewResponse(ctx)
 	type idParams struct { // Todo this is like form request in laravel
 		ID uint `uri:"id" binding:"required,min=1"`
 	}
 
 	var params idParams
 
-	if err := c.ShouldBindUri(&params); err != nil {
-		controller.SendBadRequestWithErr(errors.New("Invalid ID " + err.Error()))
+	if err := ctx.ShouldBindUri(&params); err != nil {
+		resp.Error().SendBadRequestWithErr(errors.New("Invalid ID " + err.Error()))
 		return
 	}
 
@@ -93,31 +92,32 @@ func (controller *ApplicationGroupController) Show(c *gin.Context) {
 
 	if result.Error != nil {
 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			controller.SendNotFoundWithErr(
+			resp.Error().SendNotFoundWithErr(
 				errors.New("application group not found"),
 			)
 
 			return
 		}
 		controller.logger.Error("Error on showing application group", zap.Error(result.Error))
-		controller.SendInternalServerWithErr()
+		resp.Error().SendInternalServerWithErr()
 		return
 	}
 
-	controller.SendSuccessResponse(gin.H{"group": &group})
+	resp.Success().SendOk(gin.H{"group": &group})
 	return
 }
 
-func (controller *ApplicationGroupController) Update(c *gin.Context) {
+func (controller *ApplicationGroupController) Update(ctx *gin.Context) {
 	type idParams struct { // Todo this is like form request in laravel
 		ID   uint   `json:"id" binding:"required,min=1"`
 		Name string `json:"name" binding:"required,min=1,max=255"`
 	}
 
+	resp := response.NewResponse(ctx)
 	var params idParams
 
-	if err := c.ShouldBindUri(&params); err != nil {
-		controller.SendBadRequestWithErr(
+	if err := ctx.ShouldBindUri(&params); err != nil {
+		resp.Error().SendBadRequestWithErr(
 			errors.New("invalid ID " + err.Error()),
 		)
 		return
@@ -129,13 +129,13 @@ func (controller *ApplicationGroupController) Update(c *gin.Context) {
 
 	if result.Error != nil {
 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			controller.SendNotFoundWithErr(
+			resp.Error().SendNotFoundWithErr(
 				errors.New("application group not found"),
 			)
 			return
 		}
 		controller.logger.Error("Error on updating application group", zap.Error(result.Error))
-		controller.SendInternalServerWithErr()
+		resp.Error().SendInternalServerWithErr()
 
 		return
 	}
@@ -143,20 +143,21 @@ func (controller *ApplicationGroupController) Update(c *gin.Context) {
 	group.Name = params.Name
 	database.DbConnection().Db().Save(&group)
 
-	controller.SendSuccessResponse(gin.H{"group": &group})
+	resp.Success().SendOk(gin.H{"group": &group})
 	return
 }
 
-func (controller *ApplicationGroupController) Delete(c *gin.Context) {
+func (controller *ApplicationGroupController) Delete(ctx *gin.Context) {
 
+	resp := response.NewResponse(ctx)
 	type idParams struct { // Todo this is like form request in laravel
 		ID uint `uri:"id" binding:"required,min=1"`
 	}
 
 	var params idParams
 
-	if err := c.ShouldBindUri(&params); err != nil {
-		controller.SendBadRequestWithErr(errors.New("Invalid ID " + err.Error()))
+	if err := ctx.ShouldBindUri(&params); err != nil {
+		resp.Error().SendBadRequestWithErr(errors.New("Invalid ID " + err.Error()))
 		return
 	}
 
@@ -166,15 +167,15 @@ func (controller *ApplicationGroupController) Delete(c *gin.Context) {
 
 	if result.Error != nil {
 		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-			controller.SendBadRequestWithErr(errors.New("application group not found"))
+			resp.Error().SendBadRequestWithErr(errors.New("application group not found"))
 			return
 		}
 
 		controller.logger.Error("error on deleting group", zap.Uint("group ID", group.ID), zap.Error(result.Error))
-		controller.SendInternalServerWithErr()
+		resp.Error().SendInternalServerWithErr()
 		return
 	}
 
 	database.DbConnection().Db().Delete(&group)
-	controller.SendNotContentResponse()
+	resp.Success().SendNoContent()
 }
--- api/internal/response/response.go
+++ api/internal/response/response.go
@@ -1,6 +1,27 @@
 package response
 
+import "github.com/gin-gonic/gin"
+
 type Response struct {
-	ApiSuccessResponse
-	ApiErrorResponse
+	error   *ApiErrorResponse
+	success *ApiSuccessResponse
+	ctx     *gin.Context
+}
+
+func NewResponse(ctx *gin.Context) *Response {
+	return &Response{
+		error:   NewApiErrorResponse(),
+		success: NewApiSuccessResponse(),
+		ctx:     ctx,
+	}
+}
+
+func (response *Response) Error() *ApiErrorResponse {
+	response.error.SetContext(response.ctx)
+	return response.error
+}
+
+func (response *Response) Success() *ApiSuccessResponse {
+	response.success.SetContext(response.ctx)
+	return response.success
 }
